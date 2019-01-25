23e9d221765e5d4d07e9b1a8e84ecb12a06e7d5f
Daniel Kunicki <dankunicki@gmail.com>
2019-01-26 00:24:36 +0100

    Sample scene and basic movement

---
+++ Polyjam19/Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float leftBorder;
+    public float rightBorder;
+
+    public float speed = 2f;
+
+
+    void Update()
+    {
+        float deltaTime = Time.deltaTime;
+        float distance = speed * deltaTime;
+        float directionValue = 0f;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            directionValue = -1f;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            directionValue = 1f;
+        }
+
+        Move(distance * directionValue);
+    }
+
+    void Move(float distance)
+    {
+        float newPosition = transform.position.x + distance;
+        if (newPosition > leftBorder && newPosition < rightBorder)
+            transform.Translate(new Vector3(distance, 0f, 0f));
+    }
+}

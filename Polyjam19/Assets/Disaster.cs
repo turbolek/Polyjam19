c5732cfc3fbe3c769e733efe1abb08ed79dc2c1b
Daniel Kunicki <dankunicki@gmail.com>
2019-01-26 15:03:32 +0100

    Signaling disasters

--- Polyjam19/Assets/Disaster.cs
+++ Polyjam19/Assets/Disaster.cs
@@ -6,6 +6,7 @@ public class Disaster : MonoBehaviour
 {
     public Item.Type requiredItemType;
     public Apartment apartment;
+    DisasterSpawner spawner;
 
 
     public void Interact (Item item)
@@ -14,7 +15,18 @@ public class Disaster : MonoBehaviour
             return;
         if (item.type != requiredItemType)
             return;
+        FixDisaster();
+        
+    }
 
+    void FixDisaster()
+    {
+        spawner.Reset();
         Destroy(gameObject);
+    }
+
+    public void SetSpawner(DisasterSpawner sp)
+    {
+        spawner = sp;
     }
 }
--- Polyjam19/Assets/DisasterSpawner.cs
+++ Polyjam19/Assets/DisasterSpawner.cs
@@ -8,31 +8,67 @@ public class DisasterSpawner : MonoBehaviour
     Disaster disaster;
     Apartment apartment;
     public float disasterChancePerSecond = 0.05f;
-    float rollTimer = 0f;
+    float timer = 0f;
+    public float signalingTime = 3f;
+    bool signaling = false;
+    public Sprite signalingSprite;
+    Sprite idleSprite;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        idleSprite = spriteRenderer.sprite;
+    }
 
     void Update()
     {
-        if (disaster != null)
+        if (disaster != null || signaling)
             return;
 
-        if (rollTimer > 1f)
+        if (timer > 1f)
         {
-            rollTimer = 0f;
+            timer = 0f;
             float diceRoll = Random.value;
             if (diceRoll < disasterChancePerSecond)
             {
-                GameObject disasterGO = Instantiate(disasterPrefab);
-                disaster = disasterGO.GetComponent<Disaster>();
-                disasterGO.transform.position = transform.position;
-                disaster.apartment = apartment;
+                StartCoroutine(SignalDisaster());
             }
         }
         else
-            rollTimer += Time.deltaTime;
+            timer += Time.deltaTime;
     }
 
     public void SetApartment(Apartment ap)
     {
         apartment = ap;
+    }
+
+    void SpawnDisaster()
+    {
+        GameObject disasterGO = Instantiate(disasterPrefab);
+        disaster = disasterGO.GetComponent<Disaster>();
+        disasterGO.transform.position = transform.position;
+        disaster.apartment = apartment;
+        disaster.SetSpawner(this);
+    }
+
+    IEnumerator SignalDisaster()
+    {
+        signaling = true;
+        spriteRenderer.sprite = signalingSprite;
+
+        while (signaling && timer < signalingTime)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        SpawnDisaster();
+    }
+
+    public void Reset()
+    {
+        timer = 0f;
+        spriteRenderer.sprite = idleSprite;
     }
 }

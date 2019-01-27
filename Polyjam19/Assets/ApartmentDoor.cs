e092741e64924cf8d5ac5377c48dfd319b6df1d6
Daniel Kunicki <dankunicki@gmail.com>
2019-01-27 02:05:32 +0100

    opening and closing doors

--- Polyjam19/Assets/ApartmentDoor.cs
+++ Polyjam19/Assets/ApartmentDoor.cs
@@ -6,15 +6,25 @@ public class ApartmentDoor : BaseDoor
 {
     public Apartment apartment;
     public Apartment corridor;
+    public Sprite openedSprite;
+    SpriteRenderer spriteRenderer;
+    Sprite closedSprite;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        closedSprite = spriteRenderer.sprite;
+    }
 
     public override void Enter(Player player)
     {
         apartment.Enter(player);
+        spriteRenderer.sprite = openedSprite;
     }
 
     public override void Exit(Player player)
     {
         corridor.Enter(player);
-
+        spriteRenderer.sprite = closedSprite;
     }
 }
